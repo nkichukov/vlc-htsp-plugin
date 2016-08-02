9eaa445e44d73c59e4fea9f8ebf20dfe29b53603
Thomas Leister <thomas@thomas-nb.fritz.box>
2016-08-02 10:07:40 +0200

    Fixes URL issue

--- access.cpp
+++ access.cpp
@@ -363,7 +363,8 @@ bool SubscribeHTSP(demux_t *demux)
 bool parseURL(demux_t *demux)
 {
     demux_sys_t *sys = demux->p_sys;
-    const char *path = demux->psz_location;
+    char *path;
+    asprintf( &path, "%s://%s", demux->psz_access, demux->psz_location );
 
     if(path == 0 || *path == 0)
         return false;
